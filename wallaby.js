8cfbef4f27fe62fa88c4b44c473655edd0584299
Hieu Do <mr.hieudt@gmail.com>
2017-07-23 16:41:50 -0500

    Add jasmine and wallaby configuration file

---
+++ wallaby.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+  return {
+    files: [
+      '**/*.js',
+      '!**/*.spec.js',
+    ],
+    tests: [
+      '**/*.spec.js',
+    ],
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
+    testFramework: 'jasmine'
+  };
+};
