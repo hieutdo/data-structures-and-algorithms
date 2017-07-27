38b97e421a09542c860083134c49229969e11d31
Hieu Do <mr.hieudt@gmail.com>
2017-07-27 18:35:39 -0500

    Implement fibonacci function

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -8,3 +8,8 @@ output   0 1 1 2 3 5 8 13 21 34 ...
 
 What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
 */
+
+function fibo(n) {
+  if (n === 0 || n === 1) return n;
+  return fibo(n - 1) + fibo(n - 2);
+}
