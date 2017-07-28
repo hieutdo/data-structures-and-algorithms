c454ae26c6e71d7539c61ced79ed71ba080a9f52
Hieu Do <mr.hieudt@gmail.com>
2017-07-28 15:52:46 -0500

    Implement bubble sort

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -19,3 +19,30 @@ Variants:
 (https://en.wikipedia.org/wiki/Cocktail_sort)
 
 */
+function bubbleSort(arr) {
+  let swapped = false;
+  let wall = arr.length;
+
+  function swap(arr, i, j) {
+    const tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+
+  do {
+    swapped = false;
+    for (let i = 0; i < wall - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        swapped = true;
+      }
+    }
+    wall--;
+  } while (swapped && wall > 0);
+
+  return arr;
+}
+// time complexity: O(n^2)
+
+console.log(bubbleSort([4, 2, 5, 1, 3]));
+console.log(bubbleSort([1, 3, 2, 4, 5]));
