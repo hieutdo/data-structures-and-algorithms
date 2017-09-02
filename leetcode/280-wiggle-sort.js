76876f49ce9d94aa0d9b38d2edb070f6a20a1be0
Hieu Do <mr.hieudt@gmail.com>
2017-09-02 11:37:49 -0500

    Update leetcode 280-wiggle-sort

--- leetcode/280-wiggle-sort.js
+++ leetcode/280-wiggle-sort.js
@@ -6,15 +6,23 @@ Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >
 For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
  */
 
+function swap(nums, i, j) {
+  [nums[i], nums[j]] = [nums[j], nums[i]];
+}
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const wiggleSort = function (nums) {
-  nums.sort();
-  for (let i = 1; i < nums.length - 1; i = i + 2) {
-    [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+function wiggleSort(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (i % 2 === 0) {
+      if (nums[i] > nums[i + 1]) {
+        swap(nums, i, i + 1);
+      }
+    } else if (nums[i] < nums[i + 1]) {
+      swap(nums, i, i + 1);
+    }
   }
-};
+}
 
 module.exports = wiggleSort;
