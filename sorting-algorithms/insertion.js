6a8ed5735b2b641814578c58dec1fdf7b1869efe
Hieu Do <mr.hieudt@gmail.com>
2017-09-19 12:11:14 -0500

    Implement insertion sort

--- sorting-algorithms/insertion.js
+++ sorting-algorithms/insertion.js
@@ -29,3 +29,17 @@ now repeat for next unsorted element
 (https://en.wikipedia.org/wiki/Shellsort)
 
 */
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+
+console.log(insertionSort([5, 4, 3, 2, 1]));
