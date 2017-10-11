3c63bda0623d8cc38ff6ee98613475b25a9fcb54
Hieu Do <mr.hieudt@gmail.com>
2017-10-10 17:04:37 -0700

    Update new way to check available rooms for meeting-rooms-ii

--- leetcode/253-meeting-rooms-ii.js
+++ leetcode/253-meeting-rooms-ii.js
@@ -20,40 +20,42 @@ return 2.
  * @return {number}
  */
 function minMeetingRooms(intervals) {
+  if (!intervals || intervals.length === 0) {
+    return 0;
+  }
   const starts = [];
   const ends = [];
-  let s = 0;
-  let e = 0;
-  let rooms = 0;
-  let available = 0;
-  intervals.forEach(i => {
-    starts.push(i.start);
-    ends.push(i.end);
+
+  intervals.forEach(({ start, end }) => {
+    starts.push(start);
+    ends.push(end);
   });
+
   starts.sort((a, b) => a - b);
   ends.sort((a, b) => a - b);
-  while (s < starts.length) {
-    if (starts[s] < ends[e]) {
-      if (available === 0) {
-        rooms++;
-      } else {
-        available--;
-      }
-      s++;
-    } else {
-      available++;
-      e++;
+
+  let rooms = 0;
+  let pre = 0;
+
+  for (let i = 0; i < intervals.length; i++) {
+    rooms++;
+    if (starts[i] >= ends[pre]) {
+      rooms--;
+      pre++;
     }
   }
+
   return rooms;
 }
 
 const Interval = require('./Interval');
 
-minMeetingRooms([
-  new Interval(0, 30),
-  new Interval(5, 10),
-  new Interval(15, 20)
-]);
+console.log(
+  minMeetingRooms([
+    new Interval(0, 30),
+    new Interval(5, 10),
+    new Interval(15, 20),
+  ]) === 2
+);
 
 module.exports = minMeetingRooms;
